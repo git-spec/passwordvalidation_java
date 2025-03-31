d7a248fdf64104f3067f20d160c44305dba38904
git-spec <inkyfischer@gmail.com>
2025-03-31 15:04:21 +0200

    feat: Created method validateOriginality.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Main {
+    static String[] usuals = {"1234qwer", "12345678", "password", "password1", "qwerty123", "1q2w3e4r"};
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(Main.validateOriginality("Hello World!"));
     }
 
     public static boolean validateLength(String input) {
@@ -15,5 +19,16 @@ public class Main {
 
     public static boolean validateCapitals(String input) {
         return !input.equals(input.toLowerCase());
+    }
+
+    public static boolean validateOriginality(String input) {
+        boolean isOriginal = true;
+        for (String usual: usuals) {
+            if (Objects.equals(usual, input)) {
+                isOriginal = false;
+                break;
+            }
+        }
+        return isOriginal;
     }
 }

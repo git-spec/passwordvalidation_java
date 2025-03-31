9b87b159eae30c7ac1d74d47d0e8e407a7a93647
git-spec <inkyfischer@gmail.com>
2025-03-31 16:03:01 +0200

    docs: Added documentation to class Main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,18 +9,38 @@ public class Main {
         System.out.println(Main.validateOriginality("Hello World!"));
     }
 
+    /**
+    * Validates the length of an input.
+    * @param input  input of user
+    * @return  true if length is equal or more than 8
+    */
     public static boolean validateLength(String input) {
         return input.length() >= 8;
     }
 
+    /**
+     * Validates input on digits.
+     * @param input  input of user
+     * @return  true if value includes digits
+     */
     public static boolean validateDigits(String input) {
         return input.matches(".*\\d.*");
     }
 
+    /**
+     * Validates input on capitals.
+     * @param input  input of user
+     * @return  true if value includes capitals
+     */
     public static boolean validateCapitals(String input) {
         return !input.equals(input.toLowerCase());
     }
 
+    /**
+     * Validates input on originality.
+     * @param input  input of user
+     * @return  true if value is original
+     */
     public static boolean validateOriginality(String input) {
         boolean isOriginal = true;
         for (String usual: usuals) {
@@ -32,6 +52,11 @@ public class Main {
         return isOriginal;
     }
 
+    /**
+     * Validates input on special characters.
+     * @param input  input of user
+     * @return  true if value has no special characters
+     */
     public static boolean validateSpecialChars(String input) {
         return input.matches("[A-Za-z0-9]*");
     }
